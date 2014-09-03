1e8d650621e1f33bde1661b98d020306e0d0832c
Joonas Aalto <joonas@Joonass-MacBook-Pro.local>
2014-09-03 17:37:56 +0300

    Initial commit.

---
+++ bono.js
@@ -0,0 +1,8 @@
+goog.addDependency("base.js", ['goog'], []);
+goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.object', 'goog.string.StringBuffer', 'goog.array']);
+goog.addDependency("../clojure/browser/event.js", ['clojure.browser.event'], ['cljs.core', 'goog.events.EventTarget', 'goog.events.EventType', 'goog.events']);
+goog.addDependency("../clojure/browser/net.js", ['clojure.browser.net'], ['goog.net.xpc.CfgFields', 'goog.net.XhrIo', 'goog.json', 'goog.Uri', 'cljs.core', 'goog.net.EventType', 'goog.net.xpc.CrossPageChannel', 'clojure.browser.event']);
+goog.addDependency("../clojure/browser/repl.js", ['clojure.browser.repl'], ['cljs.core', 'clojure.browser.net', 'clojure.browser.event']);
+goog.addDependency("../om/dom.js", ['om.dom'], ['cljs.core']);
+goog.addDependency("../om/core.js", ['om.core'], ['cljs.core', 'om.dom', 'goog.ui.IdGenerator']);
+goog.addDependency("../bono/core.js", ['bono.core'], ['clojure.browser.repl', 'cljs.core', 'om.dom', 'om.core']);
---
+++ src/bono/core.cljs
@@ -0,0 +1,37 @@
+(ns bono.core
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]
+            [clojure.browser.repl :as repl]))
+
+(enable-console-print!)
+
+(def app-state
+  (atom
+   {:items
+    [{:name "Blue shirt" :price 35}
+     {:name "Green trousers" :price 55}
+     {:name "Beanie hat" :price 30}
+    ]}))
+
+(defn display-item [{:keys [name price]}]
+  (str "Item: " name " price: " price))
+
+(defn item-view [item owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/li nil (display-item item)))))
+
+(defn item-list [app owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/div nil
+        (dom/h2 nil "Items")
+        (apply dom/ul nil
+          (om/build-all item-view (:items app)))))))
+
+(om/root
+ item-list
+ app-state
+ {:target (. js/document (getElementById "app"))})
