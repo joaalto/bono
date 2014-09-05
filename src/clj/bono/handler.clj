1a1203bd58f1e8696d37fd8836616a7d6bd9ee2c
Joonas Aalto <joonas.aalto@wunderdog.fi>
2014-09-05 08:53:19 +0300

    Add post function for items.

--- src/clj/bono/handler.clj
+++ src/clj/bono/handler.clj
@@ -4,10 +4,16 @@
             [compojure.route :as route]
             [ring.util.response :refer [resource-response response]]
             [ring.middleware.json :as json]
+            [bono.db :as db]
             ))
+
+(defn json-response []
+  )
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (GET "/items" [] (db/find-items []))
+  (POST "/items" {params :params} (db/insert-item params) )
   (route/resources "/")
   (route/not-found "Not Found"))
 
